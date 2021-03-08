f066aecc8f6c7da5c22721a338e60784fe75387b
chilechile <amontgomery313@noscihigh.org>
2021-03-08 17:45:36 +0000

    re commiting the assignment

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 Declare a variable named hobbies and assign it to an array.
 Then, fill the array with string values of your hobbies
 */
-
+var hobbies = ["sleeping"];
 
 
 
@@ -19,7 +19,9 @@ Then, fill the array with string values of your hobbies
 /*
 Declare a variable named dataTypes and assign it to an array.
 
+
 Fill the array with values of all data types we have learned so far, including arrays!
 
 Data Types include: String, Number, Boolean, and Array.
 */
+var dataTypes = ["String" + "Number" + "Boolean" + "Array"]; 
